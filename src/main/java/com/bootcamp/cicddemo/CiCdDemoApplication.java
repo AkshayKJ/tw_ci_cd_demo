33f2c9e6342e895f30b4e2b5f00ae0f2a4e200e1
Akshay Kumar <akshayappleinc28@gmail.com>
2021-12-03 14:45:22 +0530

    Add. Rick Roll

--- src/main/java/com/bootcamp/cicddemo/CiCdDemoApplication.java
+++ src/main/java/com/bootcamp/cicddemo/CiCdDemoApplication.java
@@ -15,7 +15,8 @@ public class CiCdDemoApplication {
 
 	@GetMapping("/")
 	public String homepage() {
-		return "<h1>My fancy homepage!</h1>";
+		return "<div style=\"position:absolute;margin:auto;\">" +
+				"<iframe width=\"420\" height=\"315\" src=\"https://drive.google.com/file/d/1Ti05_eOSGIzLkECjEjfIbiXWnMNBdIeA/preview?autoplay=1\">" +
+				"</iframe> </div>";
 	}
-
 }
