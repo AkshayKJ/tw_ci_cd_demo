5ab168bfc217d6e445b142e1da8d7464d1d3c520
Akshay Kumar <akshayappleinc28@gmail.com>
2021-12-06 09:33:05 +0530

    Refactor. Centre video div

--- src/main/java/com/bootcamp/cicddemo/CiCdDemoApplication.java
+++ src/main/java/com/bootcamp/cicddemo/CiCdDemoApplication.java
@@ -15,8 +15,17 @@ public class CiCdDemoApplication {
 
 	@GetMapping("/")
 	public String homepage() {
-		return "<div style=\"width:700;height:500;position:absolute;margin:auto;top:0;left:0;bottom:0;right:0;\">" +
-				"<iframe src=\"https://drive.google.com/file/d/1Ti05_eOSGIzLkECjEjfIbiXWnMNBdIeA/preview\" " +
-				"width=\"640\" height=\"480\" allow=\"autoplay\"></iframe></div>";
+		return "<body style=\"background-image:url('https://i.pinimg.com/originals/92/90/50/9290509b91c6a9517f83e204802a2aa2.gif');\"><div style=\"width:700;height:500;position:absolute;margin:auto;top:0;left:0;bottom:0;right:0;\">" +
+				"<video id=\"vid\" autoplay controls muted playsinline onload=\"toggleMute()\"> " +
+				"<source src=\"https://drive.google.com/u/1/uc?id=1Ti05_eOSGIzLkECjEjfIbiXWnMNBdIeA&export=download\" type=\"video/mp4\"> " +
+				"</video></div></body>" +
+				"<script> function toggleMute() {" +
+				"var video = document.getElementById(\"vid\") " +
+				"setTimeout(function () {" +
+				"if(video.muted){" +
+				" video.muted = false;" +
+				"}}, 5000);" +
+				"}</script>";
+
 	}
 }
